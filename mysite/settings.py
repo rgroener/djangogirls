2ab1a6de076695f4ff9b6315f3cfa140fc405392
rgroener <username@users.noreply.github.com>
2018-04-01 09:11:15 +0000

    added ckeditor for user(staff) and admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,10 +135,10 @@ CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
-
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': None,
+        'skin': 'moono',
     },
 }
 
